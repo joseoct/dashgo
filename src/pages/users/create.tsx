d03d77660dcbfcb286761b68a882608d9046cecc
Jose <joseoctavioconti@gmail.com>
2022-02-21 15:21:56 -0300

    Added create user function

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,17 +1,19 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
 import Link from 'next/link';
-
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from 'react-query';
+import { useRouter } from 'next/router';
 
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
 
 type CreateUserFormData = {
-  name: string
-  email: string;
+  name: string;
   password: string;
   password_confirmation: string;
 }
@@ -26,17 +28,40 @@ const signInFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
-  const { register, handleSubmit, formState: { errors, isSubmitting }, } = useForm({
+
+  const router = useRouter();
+
+  const createUser = useMutation(async (user:CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users');
+    }
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
     resolver: yupResolver(signInFormSchema),
   });
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (data, event) => {
-    event.preventDefault();
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values,
+  ) => {
+    await createUser.mutateAsync(values);
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    router.push('/users');
+  };
 
-    console.log(data);
-  }
 
   return (
     <Box>

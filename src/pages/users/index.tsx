ba8d18c11f2ab7574792793e739940f9aa9b5466
Jose <joseoctavioconti@gmail.com>
2022-02-20 22:32:27 -0300

    React query added

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,10 +12,13 @@ import {
   Thead,
   Tr,
   Text,
-  useBreakpointValue
+  useBreakpointValue,
+  Spinner
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useEffect } from 'react';
 import { RiAddLine } from 'react-icons/ri';
+import {  useQuery } from 'react-query'; 
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
@@ -23,11 +26,39 @@ import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
 
+  const { data, isLoading, error } = useQuery('users', async() => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json();
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: new Date(user.created_at).toLocaleDateString('pt-Br', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+      }
+    });
+
+    return users;
+  }, {
+    staleTime: 5000,
+  })
+
   const isLg = useBreakpointValue({
     base: false,
     lg: true,
   })
 
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+  
   return (
     <Box>
       <Header />
@@ -54,35 +85,50 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["2", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isLg && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["2", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">José Octávio</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      joseoctavioconti@gmail
-                    </Text>
-                  </Box>
-                </Td>
-                {isLg && <Td>04 de Abril, 2021</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['2', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isLg && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                    <Td px={['2', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">{user.name}</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          {user.email}
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isLg && <Td>{user.created_at}</Td>}
+                  </Tr>
+                  ))}
+                  
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,9 +1,10 @@
-import { createServer, Model } from 'miragejs';
+import { createServer, Factory, Model } from 'miragejs';
+import faker from 'faker';
 
 type User = {
   name: string;
   email: string;
-  create_at: string;
+  created_at: string;
 };
 
 export function makeServer() {
@@ -12,12 +13,30 @@ export function makeServer() {
       user: Model.extend<Partial<User>>({})
     },
 
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i+1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        created_at() {
+          return faker.date.recent(10);
+        }
+      })
+    },
+
+    seeds(server) {
+      server.createList('user', 10);
+    },
+
     routes() {
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users')
-      this.post('/users')
+      this.get('/users');
+      this.post('/users');
 
       this.namespace = '';
       this.passthrough();
